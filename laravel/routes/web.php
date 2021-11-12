1282c6482b934453c70c07961953c84d63c2ad34
zDimaBY <zdimaby@gmail.com>
2021-11-12 23:03:34 +0200

    controller and router

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', 'users', 301);
+Route::resource('users', UserController::class);
