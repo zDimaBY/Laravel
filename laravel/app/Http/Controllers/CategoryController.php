30ce4b0eedbdd242f1e88c9d44bef303439e8666
zDimaBY <zdimaby@gmail.com>
2021-11-19 15:06:00 +0200

    up Category controller

--- laravel/app/Http/Controllers/CategoryController.php
+++ laravel/app/Http/Controllers/CategoryController.php
@@ -9,7 +9,27 @@ class CategoryController extends Controller
 {
     public function index()
     {
-        $category = category::all();
-        dd($category);
+        /*$category = new category();
+        $qq = $category->products();
+        dd($qq);*/
     }
 }
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    $comments = category::find(2)->products;
+    dd($comments);
+    ?>
+</body>
+
+</html>
