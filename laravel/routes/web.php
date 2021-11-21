3ebc552d2f9d35aa52b9c95cb5b44bd75bd0f37e
zDimaBY <zdimaby@gmail.com>
2021-11-21 20:48:34 +0200

    Connecting authorization

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -15,3 +15,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::redirect('/', 'users', 301);
 Route::resource('users', UserController::class);
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
